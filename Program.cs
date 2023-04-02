2f04f7dfe29c3dd00da4fb9dd776b0e2194dc680
Romraev <romraev@yandex.ru>
2023-04-02 15:35:13 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Подготовил Раевский Р.Ю.
+
+// Task 52. Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Введите число строк: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число столбцов: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите минимальное число: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное число: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+
+    return array;
+}
+
+void Write2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int count = 0; count < array.GetLength(1) - 1; count++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] myArray = CreateRandom2dArray();
+Write2dArray(myArray);
+SortArray(myArray);
+Write2dArray(myArray);

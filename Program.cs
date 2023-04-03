3dcf5b6af8eb6f88435cd75fa297fb5f411e5054
Romraev <romraev@yandex.ru>
2023-04-03 21:15:26 +0300

    fourth task

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ SortArray(myArray);
 Write2dArray(myArray);
 */
 
-// Task 58. Задайте прямоугольный двумерный массив. 
+// Task 56. Задайте прямоугольный двумерный массив. 
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
 /* int[,] CreateRandom2dArray()
@@ -120,7 +120,7 @@ Console.WriteLine($"Строка с наименьшей суммой элеме
 
 // Task 58. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-int[,] CreateRandom2dArray()
+/* int[,] CreateRandom2dArray()
 {
     Console.Write("Введите число строк: ");
     int rows = Convert.ToInt32(Console.ReadLine());
@@ -171,7 +171,7 @@ void Write2dArray2(int[,] array1, int[,] array2)
 
         Console.WriteLine();
     }
-    //Console.WriteLine();
+    Console.WriteLine();
 }
 
 int[,] MultiplyMatrix(int[,] array1, int[,] array2)
@@ -185,7 +185,9 @@ int[,] MultiplyMatrix(int[,] array1, int[,] array2)
     return result;
 }
 
+Console.WriteLine("Введите данные для 1-ой матрицы..");
 int[,] myArray1 = CreateRandom2dArray();
+Console.WriteLine("Введите данные для 2-ой матрицы..");
 int[,] myArray2 = CreateRandom2dArray();
 if (myArray1.GetLength(1) != myArray2.GetLength(0)) Console.WriteLine("Эти матрицы нельзя перемножить!");
 else
@@ -194,3 +196,87 @@ else
     Write2dArray(myArray2);
     Write2dArray2(myArray1, multipliedArray);
 }
+*/
+
+// Task 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+/* int[,,] CreateRandom3dArray(int row, int column, int level)
+{
+    int[,,] array = new int[row, column, level];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            for (int k = 0; k < level; k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+                for (int l = 0; l < array.GetLength(0); l++)
+                {
+                    for (int m = 0; m < array.GetLength(1); m++)
+                        for (int n = 0; n < array.GetLength(2); n++)
+                        {
+                            if (l == i && m == j && n == k) break;
+                            else if (array[i, j, k] == array[l, m, n])
+                            {
+                                array[i, j, k] = new Random().Next(10, 100);
+                                l = 0;
+                                m = 0;
+                                n = 0;
+                            }
+                        }
+                }
+            }
+    return array;
+}
+
+void Write3dArray(int[,,] array) // Жду доп. балл за старания с выводом
+{
+    for (int s = 1; s <= array.GetLength(1); s++)
+        Console.Write("  ");
+    for (int c = 1; c <= array.GetLength(1); c++)
+        Console.Write("/" + c + " ");
+
+    Console.WriteLine();
+
+    for (int l = 1; l <= array.GetLength(1) * 2 - 1; l++)
+        Console.Write("___");
+
+    Console.WriteLine();
+
+    for (int k = 0; k < array.GetLength(2); k++)
+    {
+        Console.WriteLine();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int sp = 1; sp <= array.GetLength(1) * 2 - i - 3; sp++)
+                Console.Write(" ");
+
+            Console.Write(i + 1 + "/ ");
+
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j, k] + " ");
+
+            Console.WriteLine();
+        }
+        for (int l = 1; l <= array.GetLength(1) * 2 - 1; l++)
+            Console.Write("___");
+        Console.Write(k + 1 + " ");
+        Console.WriteLine();
+    }
+
+}
+
+Console.Write("Введите число строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число этажей: ");
+int levels = Convert.ToInt32(Console.ReadLine());
+
+if (rows * columns * levels > 100) Console.WriteLine("Слишком большой массив!");
+else
+{
+    int[,,] myArray = CreateRandom3dArray(rows, columns, levels);
+    Write3dArray(myArray);
+}
+*/
